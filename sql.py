f6f9b4ef6823d534af7243f85bdf3bbe9f83cd1b
Chad Estioco <chadestioco@gmail.com>
2018-11-14 02:50:40 +0800

    This mirrors the problematic test as I see it but does not exhibit the same problematic behavior.

--- sql.py
+++ sql.py
@@ -18,19 +18,19 @@ class Person(Base):
     __tablename__ = "persons"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String(255))
 
 class Role(Base):
     __tablename__ = "roles"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String(255))
 
 class Project(Base):
     __tablename__ = "projects"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String(255))
 
 class PersonRole(Base):
     __tablename__ = "person_roles"
@@ -44,6 +44,23 @@ class PersonRole(Base):
     role = relationship("Role")
     project = relationship("Project")
 
+Base.metadata.create_all(engine)
+
 role1 = Role(name="leader")
 role2 = Role(name="assistant leader")
-session.add_all((role1, role2))
+role3 = Role(name="vice leader")
+project = Project(name="project")
+leader_person = Person(name="leader person")
+al_person = Person(name="assistant leader")
+vl_person1 = Person(name="vice leader1")
+vl_person2 = Person(name="vice leader2")
+session.add_all((role1, role2, role3, project, leader_person, al_person, vl_person1, vl_person2))
+session.flush()
+
+leader_pr = PersonRole(person=leader_person, role=role1, project=project)
+al_pr = PersonRole(person=al_person, role=role2, project=project)
+vl_pr1 = PersonRole(person=vl_person1, role=role3, project=project)
+vl_pr2 = PersonRole(person=vl_person2, role=role3, project=project)
+session.add_all((leader_pr, al_pr, vl_pr1, vl_pr2))
+session.flush()
+session.commit()
